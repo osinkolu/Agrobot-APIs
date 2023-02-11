dd156ccd6b527c49220673be8f9ffb2448cc86d6
Olufemi <femiosinkolu@gmail.com>
2023-02-11 13:45:39 +0100

    remove database from loading

--- app.py
+++ app.py
@@ -28,7 +28,6 @@
 }
 """
 
-import json
 from flask import Flask, request
 from classifier import classify
 import pandas as pd
@@ -39,7 +38,7 @@ Open_weathermaps_api_key = "253c41dfcc642a2a76ef235ad92ab00c"
 
 #os.environ['Open_weathermaps_api_key']
 #print(Open_weathermaps_api_key)
-all_data = pd.read_csv("Victor_Nigerian_soil_database.csv")
+#all_data = pd.read_csv("Victor_Nigerian_soil_database.csv")
 
 def get_parameters(coordinates,all=False):
     nearest_coordinates = get_nearest_coordinates(coordinates)
