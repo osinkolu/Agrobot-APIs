c98b3eb8621ead558c81b10c32fee89d925c624c
Olufemi <femiosinkolu@gmail.com>
2023-02-11 09:17:30 +0100

    destroy everything

--- app.py
+++ app.py
@@ -19,38 +19,12 @@
 }
 """
 
-import json
-from flask import Flask,render_template,request, jsonify
-from models.classifier import classify
-from easy_predict_helper import easy_parameter_finders
+from flask import Flask,request
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return("Welcome, please smile more")
-
-@app.route("/predict", methods=["GET", "POST"])
-def predict():
-    raw_data = request.get_json(force=True)
-    return (classify(raw_data))
-
-@app.route("/easy_predict", methods=["GET", "POST"])
-def easy_predict():
-    data = request.get_json(force=True)
-    coordinates = (data['lat'], data['lon'])
-    print(coordinates)
-    easy = easy_parameter_finders()
-    raw_data = easy.collate_data(coordinates)
-    return (classify(raw_data))
-
-@app.route("/data_dump", methods=["GET", "POST"])
-def data_dump():
-    data = request.get_json(force=True)
-    coordinates = (data['lat'], data['lon'])
-    print(coordinates)
-    easy = easy_parameter_finders()
-    raw_data = easy.dump_all_data(coordinates)
-    return (raw_data)
 
 
 if __name__ =="__main__":
